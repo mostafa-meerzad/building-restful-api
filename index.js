1c473305fc843e97e7713cf718a7adb6beed056e
mostafa <mostafameerzad@gmail.com>
2023-12-11 19:33:20 +0430

    Use evn for port

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const express  = require("express");
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
-    res.send("hello");
-})
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3, 4, 5, 6]);
+});
 
+// attempt to read PORT environment variable which will be set by the hosting computer
 
-app.listen(3000, () =>  {
-    console.log("server listening on port: 3000")
-})
+// to set env in windows powerShell >>> $set:VarName="Value"
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`server listening on port: ${port}`);
+});
